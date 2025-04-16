a66481ec4f2cdb8da4c96cc5b7860d8d81a58a63
wzj-zz <wzj_reverse@outlook.com>
2025-04-16 21:49:30 +0800

    feat(language): language support setup

--- lua/community.lua
+++ lua/community.lua
@@ -9,7 +9,52 @@ if vim.g.vscode then return {} end -- don't do anything in non-vscode instances
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
+
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.python" },
+  -- { import = "astrocommunity.pack.python-ruff" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.cpp" },
+  {
+    "Civitasv/cmake-tools.nvim",
+    cmd = { "CMakeSelectCwd" },
+  },
+  { import = "astrocommunity.pack.cmake" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.java" },
+  {
+    "mfussenegger/nvim-jdtls",
+    opts = {
+      settings = {
+        -- java = {
+        --   configuration = {
+        --     runtimes = {
+        --       {
+        --         name = "JavaSE-11",
+        --         path = "/usr/lib/jvm/java-11-openjdk/",
+        --         default = true,
+        --       },
+        --     },
+        --   },
+        -- },
+        format = {
+          enabled = true,
+          settings = { -- you can use your preferred format style
+            url = "https://raw.githubusercontent.com/google/styleguide/gh-pages/eclipse-java-google-style.xml",
+            profile = "GoogleStyle",
+          },
+        },
+      },
+    },
+  },
+  { import = "astrocommunity.pack.spring-boot" },
+  { import = "astrocommunity.pack.cs" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.xml" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.markdown" },
 
   { import = "astrocommunity.git.blame-nvim" },
   { import = "astrocommunity.git.git-blame-nvim" },
